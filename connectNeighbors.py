9458f66f7a1e3e275699195ea247c463e86fce1d
Tobias Kahan <tobyk100@uw.edu>
2012-11-01 11:25:38 -0700

    Added shell method definitions.

---
+++ connectNeighbors.py
@@ -0,0 +1,16 @@
+import sys
+
+def read_graph(): 
+  print "Hello"
+
+def parse_graph(edges):
+  print "Hello"
+
+def remove_duplicates(graph):
+  print "Hello"
+
+def print_results(graph):
+  print "Hello"
+
+if __name__ == "__main__":
+  read_graph()
